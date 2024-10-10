aba73bdaa4c2e8a852b9bd3ef42b277cc83e2ce0
djaus2 <davidjones@sportronics.com.au>
2024-10-10 15:40:53 +1100

    Further code refinement

--- ConsoleTextFormatLib/Fmt.cs
+++ ConsoleTextFormatLib/Fmt.cs
@@ -1,13 +1,18 @@
 ﻿namespace ConsoleTextFormat
 {
+
+
     /// <summary>
     /// Format a Console app's text
     /// <para>i.e. In Console.Write/ln()</para>
     /// <para>Bold, Underline, Foreground/Background Colors</para>
-    /// <code>eg Console.WriteLine($"{Fmt.fg(Fmt.Clr.yellow)}Hello, {Fmt.b}World!{Fmt._b}");</code>
+    /// <code>eg Console.WriteLine($"{Fmt.fg(Fmt.Col.yellow)}Hello, {Fmt.b}World!{Fmt._b}");</code>
     /// <para>Hello in yellow text, World in bold yellow</para>
+    /// For Colored Bold use the following declaration to shorten the code
+    /// <code>using B = ConsoleTextFormat.Fmt.Bold;</code>
+    /// <code>$"{B.fgRed}"</code> is the same as <code>$"{Fmt.Bold.fgRed}"</code>
     /// </summary>
-    public static class Fmt 
+    public static class Fmt
     {
         /// <summary>
         /// Clear the console
@@ -30,6 +35,10 @@
         /// Clear all formatting
         /// </summary>
         public const string clear = "\u001b[0m";
+        /// <summary>
+        /// (Alt) Clear all formatting
+        /// </summary>
+        public const string clr = "\u001b[0m";
         /// <summary>
         /// Bold
         /// </summary>
@@ -59,56 +68,95 @@
 
         private const int ifgblack = 30;
         private const int ibgblack = ifgblack + 10;
-        private const int ibfgblack = ifgblack + 30;
+        private const int ibfgblack = 90;
         private const int ibbgblack = ibfgblack + 10;
 
         /// <summary>
-        /// Foreground colors
-        /// </summary>
-        public static string fgBla  = $"\u001b[{ifgblack + (int)Clr.black}m";
-        public static string fgRed  = $"\u001b[{ifgblack + (int)Clr.red}m";
-        public static string fgGre  = $"\u001b[{ifgblack + (int)Clr.green}m";
-        public static string fgYel  = $"\u001b[{ifgblack + (int)Clr.yellow}m";
-        public static string fgblu  = $"\u001b[{ifgblack + (int)Clr.blue}m";
-        public static string fgMag  = $"\u001b[{ifgblack + (int)Clr.magenta}m";
-        public static string fgCya  = $"\u001b[{ifgblack + (int)Clr.cyan}m";
-        public static string fgWhi  = $"\u001b[{ifgblack + (int)Clr.white}m";
+        /// Foreground Black
+        /// <code>${Fmt.fgBla}Hello{Fmt.Col}</code>
+        /// </summary>    
+        public static string fgBla = $"\u001b[{ifgblack + (int)Col.black}m";
+        /// <summary>
+        /// Foreground Red
+        /// <code>${Fmt.fgRed}Hello{Fmt.Col}</code>
+        /// </summary
+        public static string fgRed = $"\u001b[{ifgblack + (int)Col.red}m";
+        /// <summary>
+        /// Foreground Green
+        /// <code>${Fmt.fgGre}Hello{Fmt.Col}</code
+        /// </summary
+        public static string fgGre = $"\u001b[{ifgblack + (int)Col.green}m";
+        /// <summary>
+        /// Foreground Yellow
+        /// <code>${Fmt.fgYel}Hello{Fmt.Col}</code
+        /// </summary
+        public static string fgYel = $"\u001b[{ifgblack + (int)Col.yellow}m";
+        /// <summary>
+        /// Foreground Blue
+        /// <code>${Fmt.fgBlu}Hello{Fmt.Col}</code
+        /// </summary
+        public static string fgblu = $"\u001b[{ifgblack + (int)Col.blue}m";
+        /// <summary>
+        /// Foreground Magent
+        /// <code>${Fmt.fgMag}Hello{Fmt.Col}</code
+        /// </summary
+        public static string fgMag = $"\u001b[{ifgblack + (int)Col.magenta}m";
+        /// <summary>
+        /// Foreground Cyan
+        /// <code>${Fmt.fgCya}Hello{Fmt.Col}</code
+        /// </summary
+        public static string fgCya = $"\u001b[{ifgblack + (int)Col.cyan}m";
+        /// <summary>
+        /// Foreground White
+        /// <code>${Fmt.fgWhi}Hello{Fmt.Col}</code
+        /// </summary
+        public static string fgWhi = $"\u001b[{ifgblack + (int)Col.white}m";
 
         /// <summary>
         /// Background colors
+        /// <code>${Fmt.bgWhi}Hello{Fmt.Col}</code
         /// </summary>
-        public static string bgBla = $"\u001b[{ibgblack + (int)Clr.black}m";
-        public static string bgRed = $"\u001b[{ibgblack + (int)Clr.red}m";
-        public static string bgGre = $"\u001b[{ibgblack + (int)Clr.green}m";
-        public static string bgYel = $"\u001b[{ibgblack + (int)Clr.yellow}m";
-        public static string bgblu = $"\u001b[{ibgblack + (int)Clr.blue}m";
-        public static string bgMag = $"\u001b[{ibgblack + (int)Clr.magenta}m";
-        public static string bgCya = $"\u001b[{ibgblack + (int)Clr.cyan}m";
-        public static string bgWhi = $"\u001b[{ibgblack + (int)Clr.white}m";
+        public static string bgBla = $"\u001b[{ibgblack + (int)Col.black}m";
+        public static string bgRed = $"\u001b[{ibgblack + (int)Col.red}m";
+        public static string bgGre = $"\u001b[{ibgblack + (int)Col.green}m";
+        public static string bgYel = $"\u001b[{ibgblack + (int)Col.yellow}m";
+        public static string bgblu = $"\u001b[{ibgblack + (int)Col.blue}m";
+        public static string bgMag = $"\u001b[{ibgblack + (int)Col.magenta}m";
+        public static string bgCya = $"\u001b[{ibgblack + (int)Col.cyan}m";
+        public static string bgWhi = $"\u001b[{ibgblack + (int)Col.white}m";
 
         /// <summary>
-        /// Bold Foreground colors
+        /// Bold fg/bg colors
+        /// <code>${Fmt.Bold.fgRed}Hello{Fmt.Col}</code>
         /// </summary>
-        public static string bfgBla = $"\u001b[{ibfgblack + (int)Clr.black}m";
-        public static string bfgRed = $"\u001b[{ibfgblack + (int)Clr.red}m";
-        public static string bfgGre = $"\u001b[{ibfgblack + (int)Clr.green}m";
-        public static string bfgYel = $"\u001b[{ibfgblack + (int)Clr.yellow}m";
-        public static string bfgblu = $"\u001b[{ibfgblack + (int)Clr.blue}m";
-        public static string bfgMag = $"\u001b[{ibfgblack + (int)Clr.magenta}m";
-        public static string bfgCya = $"\u001b[{ibfgblack + (int)Clr.cyan}m";
-        public static string bfgWhi = $"\u001b[{ibfgblack + (int)Clr.white}m";
+        public static class Bold
+        {
 
-        /// <summary>
-        /// Bold Background colors
-        /// </summary>
-        public static string bbgBla = $"\u001b[{ibbgblack + (int)Clr.black}m";
-        public static string bbgRed = $"\u001b[{ibbgblack + (int)Clr.red}m";
-        public static string bbgGre = $"\u001b[{ibbgblack + (int)Clr.green}m";
-        public static string bbgYel = $"\u001b[{ibbgblack + (int)Clr.yellow}m";
-        public static string bbgblu = $"\u001b[{ibbgblack + (int)Clr.blue}m";
-        public static string bbgMag = $"\u001b[{ibbgblack + (int)Clr.magenta}m";
-        public static string bbgCya = $"\u001b[{ibbgblack + (int)Clr.cyan}m";
-        public static string bbgWhi = $"\u001b[{ibbgblack + (int)Clr.white}m";
+            /// <summary>
+            /// Bold Foreground colors
+            /// </summary>
+            public static string fgBla = $"\u001b[{ibfgblack + (int)Col.black}m";
+            public static string fgRed = $"\u001b[{ibfgblack + (int)Col.red}m";
+            public static string fgGre = $"\u001b[{ibfgblack + (int)Col.green}m";
+            public static string fgYel = $"\u001b[{ibfgblack + (int)Col.yellow}m";
+            public static string fgblu = $"\u001b[{ibfgblack + (int)Col.blue}m";
+            public static string fgMag = $"\u001b[{ibfgblack + (int)Col.magenta}m";
+            public static string fgCya = $"\u001b[{ibfgblack + (int)Col.cyan}m";
+            public static string fgWhi = $"\u001b[{ibfgblack + (int)Col.white}m";
+
+            /// <summary>
+            /// Bold Background colors
+            /// </summary>
+            public static string bgBla = $"\u001b[{ibbgblack + (int)Col.black}m";
+            public static string bgRed = $"\u001b[{ibbgblack + (int)Col.red}m";
+            public static string bgGre = $"\u001b[{ibbgblack + (int)Col.green}m";
+            public static string bgYel = $"\u001b[{ibbgblack + (int)Col.yellow}m";
+            public static string bgblu = $"\u001b[{ibbgblack + (int)Col.blue}m";
+            public static string bgMag = $"\u001b[{ibbgblack + (int)Col.magenta}m";
+            public static string bgCya = $"\u001b[{ibbgblack + (int)Col.cyan}m";
+            public static string bgWhi = $"\u001b[{ibbgblack + (int)Col.white}m";
+        }
+
 
         //////////////////////////////////////////////////////////////
 
@@ -116,31 +164,31 @@
         /// <summary>
         /// Foreground/Background Format colors
         /// </summary>
-        public enum Clr { black = 0, red, green, yellow, blue, magenta, cyan, white };
+        public enum Col { black = 0, red, green, yellow, blue, magenta, cyan, white };
 
         /// <summary>
         /// Set foreground color
         /// </summary>
         /// <param name="clr"></param>
         /// <returns></returns>
-        public static string fg(Clr clr) { return $"\u001b[{ifgblack + (int)clr}m"; }
+        public static string fg(Col clr) { return $"\u001b[{ifgblack + (int)clr}m"; }
         /// <summary>
         /// Set background color
         /// </summary>
         /// <param name="clr"></param>
         /// <returns></returns>
-        public static string bg(Clr clr) { return $"\u001b[{ibgblack + (int)clr}m"; }
+        public static string bg(Col clr) { return $"\u001b[{ibgblack + (int)clr}m"; }
         /// <summary>
         /// Set bold foreground color
         /// </summary>
         /// <param name="clr">Color</param>
         /// <returns></returns>
-        public static string bfg(Clr clr) { return $"\u001b[{ibfgblack + (int)clr}m"; }
+        public static string Bfg(Col clr) { return $"\u001b[{ibfgblack + (int)clr}m"; }
         /// <summary>
         /// Set bold background color
         /// </summary>
         /// <param name="clr">Color</param>
         /// <returns></returns>
-        public static string bbg(Clr clr) { return $"\u001b[{ibbgblack + (int)clr}m"; }
+        public static string Bbg(Col clr) { return $"\u001b[{ibbgblack + (int)clr}m"; }
     }
 }
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,15 +1,18 @@
-﻿namespace ConsoleApp9
+﻿using B = ConsoleTextFormat.Fmt.Bold;
+namespace ConsoleApp9
 {
     using System;
     using ConsoleTextFormat;
+
     internal class Program
     {
 
         static void Main(string[] args)
         {
-            Console.WriteLine($"{Fmt.bg(Fmt.Clr.white)}Hello, {Fmt.b}World!{Fmt._b}{Fmt.clear}");
-            Console.WriteLine($"{Fmt.fg(Fmt.Clr.yellow)}Hello, {Fmt.b}World!{Fmt._b}{Fmt.clear}");
-            Console.WriteLine($"{Fmt.fg(Fmt.Clr.blue)}Hello, {Fmt.clear}World!");
+            Console.WriteLine($"{Fmt.bg(Fmt.Col.white)}Hello, {Fmt.b}World!{Fmt._b}{Fmt.clear}");
+            Console.WriteLine($"{Fmt.fg(Fmt.Col.yellow)}Hello, {Fmt.b}World!{Fmt._b}{Fmt.clear}");
+            Console.WriteLine($"{Fmt.fg(Fmt.Col.blue)}Hello, {Fmt.clear}World!");
+            Console.WriteLine($"{B.fgRed}{B.bgCya}Hello, {Fmt.clear}World!");
         }
     }
 }
